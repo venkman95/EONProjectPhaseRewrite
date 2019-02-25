5a206dae5464e684b89933b67ce2ad27287e041e
IntersticeGames <intersticegamesdev@gmail.com>
2019-02-25 12:21:40 -0600

    UI Rewrite

---
+++ WaterSampling/Assets/Resources/Scripts/TextManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TextManager : MonoBehaviour
+{
+    TextMeshProUGUI chapter1Title = GameObject.Find("Chapter1/ChapterTitle").GetComponent<TextMeshProUGUI>();
+
+
+    [SerializeField]
+    public string[] chapterTitles;
+
+    [SerializeField]
+    public string[] chapterSummaries;
+
+    public void UpdateText() {
+        chapter1Title.text = chapterTitles[0];
+    }
+}
