3e1a336436ad1c69b996317418fd97e524c55fdf
IntersticeGames <intersticegamesdev@gmail.com>
2019-02-28 15:24:21 -0600

    Continued working on StoryManager

--- WaterSampling/Assets/Resources/Scripts/StoryManager.cs
+++ WaterSampling/Assets/Resources/Scripts/StoryManager.cs
@@ -10,26 +10,51 @@ public class StoryManager : MonoBehaviour {
         Drag
     }
 
+    AudioSource audioSource;
+
+    public int currentStep;
+
     [SerializeField]
     public Step[] steps;
 
     [System.Serializable]
     public class Step : object{
         [SerializeField]
-        GameObject objectTarget;
+        public GameObject objectTarget;
         [SerializeField]
-        AudioClip audioClip;
+        public AudioClip audioClip;
         [SerializeField]
-        int stepOrder;
+        public AnimationClip animClip;
+        [SerializeField]
+        public int stepOrder;
         [SerializeField]
         public TapOrDrag tapOrDrag;
     }
 
     public void Awake() {
-        
+        audioSource = GetComponent<AudioSource>();
     }
 
-    public void playAudio() {
+    public void Update() {
+        for (var i = 0; i < Input.touchCount; ++i) {
+            if (Input.GetTouch(i).phase == TouchPhase.Began) {
+                Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(i).position);
+                RaycastHit hit;
+                if (Physics.Raycast(ray, out hit)) {
+                    foreach(Step elem in steps) {
+                        if(hit.transform.gameObject == elem.objectTarget && currentStep == elem.stepOrder && !audioSource.isPlaying) {
+                            playAudio(elem.audioClip);
+                            //play animation
+                            currentStep++;
+                        }
+                    }
+                }
+            }
+        }
+    }
 
+    public void playAudio(AudioClip audio) {
+        audioSource.clip = audio;
+        audioSource.Play();
     }
 }
