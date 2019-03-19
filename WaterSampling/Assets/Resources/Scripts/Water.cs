8911cb91036ebd97c68809a642d272dc58c16702
IntersticeGames <intersticegamesdev@gmail.com>
2019-03-19 12:22:17 -0500

    Fixed gap between water and faucet when aerator is off

--- WaterSampling/Assets/Resources/Scripts/Water.cs
+++ WaterSampling/Assets/Resources/Scripts/Water.cs
@@ -30,6 +30,7 @@ public class Water : MonoBehaviour
                 break;
             case 5:
                 water.SetActive(true);
+                water.transform.position = new Vector3(-0.251f,0.355f,0.129f);
                 break;
             case 6:
                 StartCoroutine(Lerp(0,100,1));
@@ -59,10 +60,10 @@ public class Water : MonoBehaviour
         float elapsedTime = 0;
         while (elapsedTime > time && elapsedTime < 1) {
             if (step == 8) {
-                water.transform.position = new Vector3(-0.251f,0.462f,0.129f);
+                water.transform.position = new Vector3(-0.251f,0.487f,0.129f);
                 water.transform.localScale = new Vector3(16.40755f,16.40755f,31f);
             } else if (step == 10) {
-                water.transform.position = new Vector3(-0.251f,0.3307735f,0.129f);
+                water.transform.position = new Vector3(-0.251f,0.355f,0.129f);
                 water.transform.localScale = new Vector3(16.40755f,16.40756f,57.42658f);
             }
             yield return null;
